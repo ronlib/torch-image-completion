05ffd2cf6934a536c2f73556fc54bac0c1966ce0
Ron <ronll64@gmail.com>
2018-04-20 17:53:54 +0300

    Added images, runs vote

--- run_inpaint.lua
+++ run_inpaint.lua
@@ -1,9 +1,14 @@
 require("wx")
 require("os")
 require 'torch'
-luainpaint = require("luainpaint")
+-- luainpaint = require("luainpaint")
 nnluainpaint = require("libpatchmatch2")
-patch2vec_init('model16_570.t7')
+-- patch2vec_init('model16_570.t7')
 -- result_file_path = luainpaint.inpaint('image.bmp', 'mask.bmp')
-result_file_path = nnluainpaint.inpaint('image.bmp', 'mask.bmp')
+-- nn_ab = nnluainpaint.nn('a.bmp', 'c.bmp', 'cpu', 16, 5)
+-- nn_ba = nnluainpaint.nn('c.bmp', 'a.bmp', 'cpu', 16, 5)
+nn_ab = nnluainpaint.nn('image.bmp', 'image.bmp', 'cpu', 16, 5)
+result_file_path = nnluainpaint.vote('image.bmp', nn_ab, nil, 'cpu', 16,
+																		 'mask.bmp', nil, nil, nil, nil,
+																		 'mask.bmp')
 print("Result file path: " .. result_file_path)
