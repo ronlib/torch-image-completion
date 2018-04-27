e8a351d196577c2a770c6f44b1a84696841db0f4
Ron <ronll64@gmail.com>
2018-04-27 19:56:03 +0300

    Added more parameters to when calling nn()

--- run_inpaint.lua
+++ run_inpaint.lua
@@ -7,7 +7,15 @@ nnluainpaint = require("libpatchmatch2")
 -- result_file_path = luainpaint.inpaint('image.bmp', 'mask.bmp')
 -- nn_ab = nnluainpaint.nn('a.bmp', 'c.bmp', 'cpu', 16, 5)
 -- nn_ba = nnluainpaint.nn('c.bmp', 'a.bmp', 'cpu', 16, 5)
-nn_ab = nnluainpaint.nn('image.bmp', 'image.bmp', 'cpu', 16, 5)
+nn_ab = nnluainpaint.nn('image.bmp', -- A_file_path
+												'image.bmp', -- B_file_path
+												'cpu',
+												16,
+												5,
+												nil, nil, nil, nil, nil,
+												'mask.bmp', -- 1 means a maksed pixel
+												'mask.bmp' -- 1 means a maksed pixel
+                        )
 result_file_path = nnluainpaint.vote('image.bmp', nn_ab, nil, 'cpu', 16,
 																		 'mask_inv.bmp', nil, nil, nil, nil,
 																		 'mask_inv.bmp')
